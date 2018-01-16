8d6ddc4b3e4e1d8e635ce128ec44d606ecf781cb
James McMillan <jmcmillan29@gmail.com>
2018-01-16 09:32:10 -0800

    changed the port that the app launches in to fix heroku launching the app

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.use((err, req, res, next) => {
     res.render('error')
 })
 
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!')
+app.listen(8080, () => {
+    console.log('The application is running on localhost:8080!')
 });
